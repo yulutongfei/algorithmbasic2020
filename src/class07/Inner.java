5e0acf4394efe754780737e7f857e568c68bfb25
孙许 <2572012446@qq.com>
2021-09-22 21:31:57 +0800

    tab改成4个空格

--- src/class07/Inner.java
+++ src/class07/Inner.java
@@ -1,9 +1,9 @@
 package class07;
 
 public class Inner<T> {
-	public T value;
+    public T value;
 
-	public Inner(T v) {
-		value = v;
-	}
+    public Inner(T v) {
+        value = v;
+    }
 }
--- src/class16/Edge.java
+++ src/class16/Edge.java
@@ -1,14 +1,14 @@
 package class16;
 
 public class Edge {
-	public int weight;
-	public Node from;
-	public Node to;
+    public int weight;
+    public Node from;
+    public Node to;
 
-	public Edge(int weight, Node from, Node to) {
-		this.weight = weight;
-		this.from = from;
-		this.to = to;
-	}
+    public Edge(int weight, Node from, Node to) {
+        this.weight = weight;
+        this.from = from;
+        this.to = to;
+    }
 
 }
