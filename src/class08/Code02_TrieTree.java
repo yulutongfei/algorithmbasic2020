59d29bcd6ec6028ad1003bcb356112c2a4a4f4a5
孙许 <2572012446@qq.com>
2022-02-13 22:30:05 +0800

    去除答案

--- src/class08/Code02_TrieTree.java
+++ src/class08/Code02_TrieTree.java
@@ -35,76 +35,21 @@ public class Code02_TrieTree {
         }
 
         public void insert(String word) {
-            if (word == null) {
-                return;
-            }
-            char[] str = word.toCharArray();
-            Node1 node = root;
-            node.pass++;
-            int path = 0;
-            for (int i = 0; i < str.length; i++) { // 从左往右遍历字符
-                path = str[i] - 'a'; // 由字符，对应成走向哪条路
-                if (node.nexts[path] == null) {
-                    node.nexts[path] = new Node1();
-                }
-                node = node.nexts[path];
-                node.pass++;
-            }
-            node.end++;
+
         }
 
         public void delete(String word) {
-            if (search(word) != 0) {
-                char[] chs = word.toCharArray();
-                Node1 node = root;
-                node.pass--;
-                int path = 0;
-                for (int i = 0; i < chs.length; i++) {
-                    path = chs[i] - 'a';
-                    if (--node.nexts[path].pass == 0) {
-                        node.nexts[path] = null;
-                        return;
-                    }
-                    node = node.nexts[path];
-                }
-                node.end--;
-            }
+
         }
 
         // word这个单词之前加入过几次
         public int search(String word) {
-            if (word == null) {
-                return 0;
-            }
-            char[] chs = word.toCharArray();
-            Node1 node = root;
-            int index = 0;
-            for (int i = 0; i < chs.length; i++) {
-                index = chs[i] - 'a';
-                if (node.nexts[index] == null) {
-                    return 0;
-                }
-                node = node.nexts[index];
-            }
-            return node.end;
+            return 0;
         }
 
         // 所有加入的字符串中，有几个是以pre这个字符串作为前缀的
         public int prefixNumber(String pre) {
-            if (pre == null) {
-                return 0;
-            }
-            char[] chs = pre.toCharArray();
-            Node1 node = root;
-            int index = 0;
-            for (int i = 0; i < chs.length; i++) {
-                index = chs[i] - 'a';
-                if (node.nexts[index] == null) {
-                    return 0;
-                }
-                node = node.nexts[index];
-            }
-            return node.pass;
+            return 0;
         }
     }
 
@@ -130,76 +75,21 @@ public class Code02_TrieTree {
         }
 
         public void insert(String word) {
-            if (word == null) {
-                return;
-            }
-            char[] chs = word.toCharArray();
-            Node2 node = root;
-            node.pass++;
-            int index = 0;
-            for (int i = 0; i < chs.length; i++) {
-                index = (int) chs[i];
-                if (!node.nexts.containsKey(index)) {
-                    node.nexts.put(index, new Node2());
-                }
-                node = node.nexts.get(index);
-                node.pass++;
-            }
-            node.end++;
+
         }
 
         public void delete(String word) {
-            if (search(word) != 0) {
-                char[] chs = word.toCharArray();
-                Node2 node = root;
-                node.pass--;
-                int index = 0;
-                for (int i = 0; i < chs.length; i++) {
-                    index = (int) chs[i];
-                    if (--node.nexts.get(index).pass == 0) {
-                        node.nexts.remove(index);
-                        return;
-                    }
-                    node = node.nexts.get(index);
-                }
-                node.end--;
-            }
+
         }
 
         // word这个单词之前加入过几次
         public int search(String word) {
-            if (word == null) {
-                return 0;
-            }
-            char[] chs = word.toCharArray();
-            Node2 node = root;
-            int index = 0;
-            for (int i = 0; i < chs.length; i++) {
-                index = (int) chs[i];
-                if (!node.nexts.containsKey(index)) {
-                    return 0;
-                }
-                node = node.nexts.get(index);
-            }
-            return node.end;
+            return 0;
         }
 
         // 所有加入的字符串中，有几个是以pre这个字符串作为前缀的
         public int prefixNumber(String pre) {
-            if (pre == null) {
-                return 0;
-            }
-            char[] chs = pre.toCharArray();
-            Node2 node = root;
-            int index = 0;
-            for (int i = 0; i < chs.length; i++) {
-                index = (int) chs[i];
-                if (!node.nexts.containsKey(index)) {
-                    return 0;
-                }
-                node = node.nexts.get(index);
-            }
-            return node.pass;
+            return 0;
         }
     }
 
