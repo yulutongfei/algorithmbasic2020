019f54bf608e2b6808afcbce21284f5c152c226f
孙许 <2572012446@qq.com>
2022-02-16 07:59:12 +0800

    去除答案

--- src/class01/Code03_InsertionSort.java
+++ src/class01/Code03_InsertionSort.java
@@ -5,22 +5,7 @@ import java.util.Arrays;
 public class Code03_InsertionSort {
 
     public static void insertionSort(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-        // 不只1个数
-        for (int i = 1; i < arr.length; i++) { // 0 ~ i 做到有序
-            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
-                swap(arr, j, j + 1);
-            }
-        }
-    }
 
-    // i和j是一个位置的话，会出错
-    public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
     }
 
     // for test
--- src/class04/Code01_MergeSort.java
+++ src/class04/Code01_MergeSort.java
@@ -4,72 +4,12 @@ public class Code01_MergeSort {
 
     // 递归方法实现
     public static void mergeSort1(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-        process(arr, 0, arr.length - 1);
-    }
 
-    // 请把arr[L..R]排有序
-    // l...r N
-    // T(N) = 2 * T(N / 2) + O(N)
-    // O(N * logN)
-    public static void process(int[] arr, int L, int R) {
-        if (L == R) { // base case
-            return;
-        }
-        int mid = L + ((R - L) >> 1);
-        process(arr, L, mid);
-        process(arr, mid + 1, R);
-        merge(arr, L, mid, R);
-    }
-
-    public static void merge(int[] arr, int L, int M, int R) {
-        int[] help = new int[R - L + 1];
-        int i = 0;
-        int p1 = L;
-        int p2 = M + 1;
-        while (p1 <= M && p2 <= R) {
-            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
-        }
-        // 要么p1越界了，要么p2越界了
-        while (p1 <= M) {
-            help[i++] = arr[p1++];
-        }
-        while (p2 <= R) {
-            help[i++] = arr[p2++];
-        }
-        for (i = 0; i < help.length; i++) {
-            arr[L + i] = help[i];
-        }
     }
 
     // 非递归方法实现
     public static void mergeSort2(int[] arr) {
-        if (arr == null || arr.length < 2) {
-            return;
-        }
-        int N = arr.length;
-        // 步长
-        int mergeSize = 1;
-        while (mergeSize < N) { // log N
-            // 当前左组的，第一个位置
-            int L = 0;
-            while (L < N) {
-                if (mergeSize >= N - L) {
-                    break;
-                }
-                int M = L + mergeSize - 1;
-                int R = M + Math.min(mergeSize, N - M - 1);
-                merge(arr, L, M, R);
-                L = R + 1;
-            }
-            // 防止溢出
-            if (mergeSize > N / 2) {
-                break;
-            }
-            mergeSize <<= 1;
-        }
+
     }
 
     // for test
