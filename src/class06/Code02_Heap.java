46c53897dfc8589b2a429dc4f5fe0305b58a1693
孙许 <2572012446@qq.com>
2022-03-10 22:22:03 +0800

    去除答案

--- src/class06/Code02_Heap.java
+++ src/class06/Code02_Heap.java
@@ -17,71 +17,22 @@ public class Code02_Heap {
         }
 
         public boolean isEmpty() {
-            return heapSize == 0;
+            return true;
         }
 
         public boolean isFull() {
-            return heapSize == limit;
+            return true;
         }
 
         public void push(int value) {
-            if (heapSize == limit) {
-                throw new RuntimeException("heap is full");
-            }
-            heap[heapSize] = value;
-            // value  heapSize
-            heapInsert(heap, heapSize++);
+
         }
 
         // 用户此时，让你返回最大值，并且在大根堆中，把最大值删掉
         // 剩下的数，依然保持大根堆组织
         public int pop() {
-            int ans = heap[0];
-            swap(heap, 0, --heapSize);
-            heapify(heap, 0, heapSize);
-            return ans;
+            return 0;
         }
-
-        
-        
-        
-        
-        
-        // 新加进来的数，现在停在了index位置，请依次往上移动，
-        // 移动到0位置，或者干不掉自己的父亲了，停！
-        private void heapInsert(int[] arr, int index) {
-            // [index]    [index-1]/2
-            // index == 0
-            while (arr[index] > arr[(index - 1) / 2]) {
-                swap(arr, index, (index - 1) / 2);
-                index = (index - 1) / 2;
-            }
-        }
-
-        // 从index位置，往下看，不断的下沉
-        // 停：较大的孩子都不再比index位置的数大；已经没孩子了
-        private void heapify(int[] arr, int index, int heapSize) {
-            int left = index * 2 + 1;
-            while (left < heapSize) { // 如果有左孩子，有没有右孩子，可能有可能没有！
-                // 把较大孩子的下标，给largest
-                int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
-                largest = arr[largest] > arr[index] ? largest : index;
-                if (largest == index) {
-                    break;
-                }
-                // index和较大孩子，要互换
-                swap(arr, largest, index);
-                index = largest;
-                left = index * 2 + 1;
-            }
-        }
-
-        private void swap(int[] arr, int i, int j) {
-            int tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-        }
-
     }
 
     public static class RightMaxHeap {
--- src/class13/Code05_LowestLexicography.java
+++ src/class13/Code05_LowestLexicography.java
@@ -1,7 +1,5 @@
 package class13;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Code05_LowestLexicography {
@@ -47,23 +45,16 @@ public class Code05_LowestLexicography {
         return ans;
     }
 
-    public static class MyComparator implements Comparator<String> {
-        @Override
-        public int compare(String a, String b) {
-            return (a + b).compareTo(b + a);
-        }
-    }
 
+    /**
+     * 在此方法实现
+     *
+     * @param strs
+     * @return
+     */
     public static String lowestString2(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        Arrays.sort(strs, new MyComparator());
-        String res = "";
-        for (int i = 0; i < strs.length; i++) {
-            res += strs[i];
-        }
-        return res;
+
+        return "";
     }
 
     // for test
